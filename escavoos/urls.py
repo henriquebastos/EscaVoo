39861d5372fd563737216c89b9d3e9c535b9d573
bruno santana <santanasta@gmail.com>
2016-12-11 21:30:29 -0200

    inserido comando ajax no html

--- escavoos/urls.py
+++ escavoos/urls.py
@@ -1,11 +1,12 @@
 """Define padrões de URL para escavoos"""
 
 from django.conf.urls import url
-from escavoos.views import index, escavoo
+from escavoos.views import index, escavoo, api
 
 
 urlpatterns = [
     # página inicial
     url(r'^$', index, name='index'),
     url(r'^escavoo/$', escavoo, name='escavoo'),
+    url(r'^api/$', api, name='api'),
 ]
--- escavoos/views.py
+++ escavoos/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from datetime import datetime
 from .models import Voo
+from django.http import JsonResponse
 
 
 def index(request):
@@ -13,15 +14,48 @@ def escavoo(request):
 
     # inicia com a data atual. Se o usuário selecionar uma data, mostra a escala da data selecionada
     if request.method == 'POST':
-        data = request.POST['date']
-        data = datetime.strptime(str(data), '%Y-%m-%d').date()
+        data_selecionada = request.POST['date']
+        data_selecionada = datetime.strptime(str(data_selecionada), '%Y-%m-%d').date()
 
     else:
-        data = datetime.today().date()
+        data_selecionada = datetime.today().date()
 
-    voos = Voo.objects.filter(data=str(data))
+    voos = Voo.objects.filter(data=str(data_selecionada))
     context['voos'] = voos
-    context['date'] = data
+    context['date'] = data_selecionada
     return render(request, 'escavoos/escala.html', context)
 
 
+def _get_color(voo):
+    if voo.abortado_solo:
+        return "red"
+    elif voo.hora_pso:
+        return "#6699ff"
+    elif voo.hora_dep:
+        return "#66ff99"
+    elif voo.hora_partida:
+        return "#ffff99"
+    else:
+        return "white"
+
+# api para retornar um JSON que servirá para o ajax fazer o refresh automático da tabela no html
+def api(request):
+    context = {}
+
+    # inicia com a data atual. Se o usuário selecionar uma data, mostra a escala da data selecionada
+    if request.method == 'POST':
+        data_selecionada = request.POST['date']
+        data_selecionada = datetime.strptime(str(data_selecionada), '%Y-%m-%d').date()
+
+    else:
+        data_selecionada = datetime.today().date()
+
+    voos = Voo.objects.filter(data=str(data_selecionada))
+    context['date'] = data_selecionada
+
+    context['voos'] = list()
+    for voo in voos:
+        info = {'id': voo.pk, 'color': _get_color(voo)}
+        context['voos'].append(info)
+
+    return JsonResponse(context)
